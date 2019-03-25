3114fdf87c30fa4be284e30609a5578742340fd2
fibio <fibio.tany@gmail.com>
2019-03-25 13:16:47 +0200

    Changed setup

--- setup.py
+++ setup.py
@@ -10,33 +10,36 @@ MAJOR, MINOR, MICRO = 1, 0, 3
 __VERSION__ = "{}.{}.{}".format(MAJOR, MINOR, MICRO)
 
 setup(
-    name="docstr_coverage",
+    name="django_docstr_coverage",
     version=__VERSION__,
     description=" ".join(
         [
             "Utility for examining python source files to ensure proper documentation.",
             "Lists missing docstrings, and calculates overall docstring coverage percentage rating",
+            "Adds Django framework support"
         ]
     ),
     long_description=readme(),
     long_description_content_type="text/markdown",
-    keywords="docstring coverage documentation audit source code statistics report",
-    url="https://github.com/HunterMcGushion/docstr_coverage",
-    author="Hunter McGushion",
+    keywords="docstring coverage documentation audit source code statistics report for Django",
+    url="https://github.com/banzai-io/docstr_coverage.git",
+    author="Fibio, Hunter McGushion",
     author_email="hunter@mcgushion.com",
     license="MIT",
-    packages=['docstr_coverage'],
-    install_requires=[],
+    packages=['docstr_coverage', 'management'],
+    python_requires=">=3.4",
+    install_requires=["Django>=1.11"],
     include_package_data=True,
     zip_safe=False,
     test_suite="nose.collector",
     tests_require=["nose"],
     entry_points=dict(console_scripts=["docstr-coverage=docstr_coverage.coverage:_execute"]),
     classifiers=(
-        # TODO: Check Python 2 compatibility
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
         "Topic :: Documentation",
         "Topic :: Documentation :: Sphinx",
         "Topic :: Software Development",
